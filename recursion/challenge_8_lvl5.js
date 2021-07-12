668f285bb9c77f3c60b7910b401e5126d613c71e
Ivan <kleimenov416@gmail.com>
2021-07-12 17:01:21 -0400

    add solutions

--- recursion/challenge_8_lvl5.js
+++ recursion/challenge_8_lvl5.js
@@ -9,17 +9,19 @@ let data = {
   }
 
   const dummyFunc = (dict) => {
+      
       for (let item in dict) {
+          console.log(item)
         //Object.keys(dict[item]) ? console.log(item) : dummyFunc(dict[item]);
-        if(Object.keys(item)){
-           console.log('this is a key:', item)
-           console.log('this is a value:', dict[item])
-           for (let key in dict[item]) {
-               console.log('this is a key:', key)
-               console.log('this is a value:', dict[item][key])
-           }
-           
-        }
+        // if(Object.keys(item)){
+        //    console.log('this is a key:', item)
+        //    console.log('this is a value:', dict[item])
+        //    for (let key in dict[item]) {
+        //        console.log('this is a key:', key)
+        //        console.log('this is a value:', dict[item][key])
+        //    }
+        // }
+        dummyFunc(dict[item])
       }
   }
 
