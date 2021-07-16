e5152eb4ea3a550db3a59175c70ec920da4ecaf0
Ivan <kleimenov416@gmail.com>
2021-07-16 12:44:46 -0400

    add solution

--- recursion/challenge_9_lvl6.js
+++ recursion/challenge_9_lvl6.js
@@ -1,4 +1,4 @@
-let arr = [1, 2, [1, 2]]
+let arr = [1, 2, [1, 2], '3pig3', '3function() { return 15; }3']
 
 
 //let sum = 0;
@@ -9,18 +9,31 @@ let arr = [1, 2, [1, 2]]
 //     return sum;  
 // }
 
-const ggg = (array) => {
-    let sum = 0;
-    const arraySum = (array) => {
-        array.map((item)=>
-        Array.isArray(item) ? arraySum(item): sum += item
-    )
-    return sum;
-    }
-    return arraySum(array)
-}
+
+
+// const ggg = (array) => {
+//     let sum = 0;
+//     const arraySum = (array) => {
+//         array.map((item)=> {
+//             if(typeof(item) === 'string'){
+//                 console.log(item)
+//                 sum += item.replace(/[^0-9]/g, "").split('').reduce((a,b)=> parseInt(a) + parseInt(b), 0)
+//             } else {
+//                 Array.isArray(item) ? arraySum(item): sum += item
+//             }
+//         })
+//     return sum;
+//     }
+//     return arraySum(array)
+// }
+// console.log(ggg(arr))
+
+
+let test = '3function() { return 15; }3'
+let tmp = test.replace(/[^0-9]/g, "").split('').reduce((a,b)=> parseInt(a) + parseInt(b), 0)
+
+console.log(tmp);
 
 
 
 
-console.log(ggg(arr))
