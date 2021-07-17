65bf503a3d1de41d3cfb0ffa730d888f693f2c0f
Ivan <kleimenov416@gmail.com>
2021-07-17 12:47:06 -0400

    add solution

--- level 6/challenge_2_lvl6.js
+++ level 6/challenge_2_lvl6.js
@@ -1,5 +1,20 @@
+
 const persistence = (num) => {
+    let sum = num;
+    let cnt = 0;
     
+    const sumOfNumbers = (sum) => {
+        cnt += 1;
+        sum = sum.toString()
+        sum = sum.split('')
+        sum = sum.reduce((a, b) => parseInt(a) * parseInt(b))
+        return sum.toString().split('').length > 1 ? sumOfNumbers(sum) :  sum;
+    }    
+       
+    if(num.toString().split('').length > 1){
+        sumOfNumbers(sum)
+    }
+    return cnt;
 }
 
-console.log(persistence(39))
+console.log(persistence(999))
